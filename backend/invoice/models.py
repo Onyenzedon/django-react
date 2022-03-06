20fc0f4c92647adf8bf3644416a542564ea689ac
onyenzedon@gmail.com <onyenzedon@gmail.com>
2022-03-06 09:43:02 +0100

    fullstack commit

--- backend/invoice/models.py
+++ backend/invoice/models.py
@@ -4,12 +4,23 @@ from decimal import Decimal
 # Create your models here.
 
 
+# class Invoice(models.Model):
+#     Description = models.CharField(max_length=100)
+#     Brand = models.CharField(max_length=100)
+#     Quantity = models.PositiveIntegerField()
+#     Value = models.CharField(max_length=100)
+#     Unit_Price = models.PositiveIntegerField()
+#     Total_Price = models.DecimalField(max_digits=16, decimal_places=2, default=0.0)
+
+#     def __str__(self):
+#         return self.Description
+
 class Invoice(models.Model):
-    Description = models.CharField(max_length=100)
-    Brand = models.CharField(max_length=100)
-    Quantity = models.DecimalField(max_digits=16, decimal_places=2, default=Decimal(0))
-    Value = models.CharField(max_length=100)
-    Unit_Price = models.DecimalField(max_digits=16, decimal_places=2, default=Decimal(0))
+    Description = models.CharField(max_length=100, default="")
+    Brand = models.CharField(max_length=100, default="")
+    Quantity = models.PositiveIntegerField()
+    Value = models.CharField(max_length=100, default="")
+    Unit_Price = models.PositiveIntegerField()
     # Total_Price = models.DecimalField(max_digits=16, decimal_places=2, default=0.0)
 
     def __str__(self):
